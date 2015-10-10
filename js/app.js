1569bf2a9d6bacc3f59f1ff5afb8983cc564df7f
pr0dd <sgx4321@gmail.com>
2015-10-10 16:38:40 +0300

    Adds routes: frontpage + shop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-﻿var app = angular.module("app", []);
+﻿var app = angular.module("app", [
+	"ngRoute"
+]);
 
-//app.config();
+app.config(["$routeProvider", function($routeProvider){
+	$routeProvider
+	.when("/", {
+		templateUrl: "partials/frontpage.html"
+	})
+	.when("/shop",{
+		templateUrl: "partials/shop.html"	
+	})
+	.otherwise({
+		templateUrl: "partials/frontpage.html"
+	});
+}]);
 
 app.controller("mainCtrl", ["$scope", function($scope){
 
