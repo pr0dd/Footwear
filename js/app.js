9da92b9dc0937f4f9f5f0016d0e8401c789d5827
pr0dd <sgx4321@gmail.com>
2015-10-14 12:42:19 +0300

    Adds initPrices function which uses the reworked getPrice func to initialize 'price' filter variables.Now min and max price will vary depending on selected category

--- js/app.js
+++ js/app.js
@@ -23,42 +23,60 @@ app.constant("activeClass", "active");
 
 //Main controller:
 app.controller("mainCtrl", ["$scope","$http","baseUrl", "activeClass", "$location", function($scope, $http, baseUrl, activeClass, $location){
-	//Define get function for 'price' filter:
-	var getPrice = function(max){
-		var prices = [];
-		for(var i = 0; i < $scope.data.products.length; i++){
-			var val = $scope.data.products[i].price;
-			prices.push(val);
-		}
-		return angular.isDefined(max) ? Math.max.apply(null,prices) : Math.min.apply(null,prices);
-	}
-
-	//get products from server:
-	$scope.data = {};
-	$http.get(baseUrl)
-		.success(function(data){
-			$scope.data.products = data;
-			//Define default variables which require $scope.data.products:
-			$scope.data.min = getPrice(); //get dynamic min value for price filter's ng-model directive;
-			$scope.data.max = getPrice(true); //get dynamic max value;
-			$scope.min = getPrice(); //get static min value for range input;
-			$scope.max = getPrice(true); //get static max value;
-		})
-		.error(function(error){
-			$scope.data.error
-		});
-		
-	//FILTERS:
-		//Define default variables:
+	//Define default variables:
 	var productCategory = null;
 	$scope.productType = [];
 	$scope.productBrand = [];
 	$scope.typeVals = []; //ng-model values for type filter:
 	$scope.brandVals = []; //ng-model values for brand filter:
+
+	//Initialization function for 'price' filter:
+	var initPrices = function(){
+		//Define get function:
+		var getPrice = function(max){
+			var prices = [];
+			var data = $scope.data.products;
+			if(productCategory==null){
+				for(var i = 0; i < data.length; i++){
+					var val = data[i].price;
+					prices.push(val);
+				}
+			} else {
+				for(var i = 0; i < data.length; i++){
+					if(data[i].category == productCategory) {
+						var val = data[i].price;
+						prices.push(val);
+					}
+				}
+			}
+			return angular.isDefined(max) ? Math.max.apply(null,prices) : Math.min.apply(null,prices);
+		}
+		//Get dynamic min/max values for 'price' filter's ng-model directive:
+		$scope.data.min = getPrice(); 
+		$scope.data.max = getPrice(true);
+		//Get static min/max values for range input:
+		$scope.min = getPrice(); 
+		$scope.max = getPrice(true); 
+	}
+	//Get products from server:
+	$scope.data = {};
+	$http.get(baseUrl)
+		.success(function(data){
+			$scope.data.products = data;
+			//Define defaults for 'price' filter:
+			initPrices();
+		})
+		.error(function(error){
+			$scope.data.error
+		});
+
+	//FILTERS:
 		//Selecting functions:
 	$scope.selectCategory = function(newItem){
 		productCategory = newItem;
 		$scope.refreshFilters();
+		//Update values of 'price' filter at each change of category;
+		initPrices();
 		$location.path("/shop");
 	}
 	$scope.toggleFilter = function(newItem, source){
@@ -84,13 +102,13 @@ app.controller("mainCtrl", ["$scope","$http","baseUrl", "activeClass", "$locatio
 	$scope.brandFilterFn = function(item){
 		return $scope.productBrand.length == 0 || $scope.productBrand.indexOf(item.brand) != -1;
 	}
+
 		//Refresh filters:
 	$scope.refreshFilters = function(){
 		$scope.productType.length = 0;
 		$scope.productBrand.length = 0;
 		$scope.typeVals.length = 0;
 		$scope.brandVals.length = 0;
-
 	}
 
 }]);
